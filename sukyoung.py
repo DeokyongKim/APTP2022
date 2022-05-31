046e64ab3a76cbb35909af5ab705d302e79522bd
quartz0516 <tjrdud030516@yonsei.ac.kr>
2022-05-31 22:12:38 +0900

    노가다 했습니다.

--- sukyoung.py
+++ sukyoung.py
@@ -7,66 +7,67 @@ def get_2by2(table):
 
     elif table == [[0, 0],
                    [0, 1]]:
-        ans = [[table[1][1]]]
+        ans = [[1, 1]]
 
     elif table == [[0, 0],
                    [1, 0]]:
-        ans = [[table[1][0]]]
+        ans = [[1, 0]]
 
     elif table == [[0, 0],
                    [1, 1]]:
-        ans = [[table[1][0], table[1][1]]]
+        ans = [[1, 0], [1, 1]]
 
     elif table == [[0, 1],
                    [0, 0]]:
-        ans = [[table[0][1]]]
+        ans = [[0, 1]]
 
     elif table == [[0, 1],
                    [0, 1]]:
-        ans = [[table[0][1], table[1][1]]]
+        ans = [[0, 1], [1, 1]]
 
     elif table == [[0, 1],
                    [1, 1]]:
-        ans = [[table[0][1], table[1][1]], [table[1][0], table[1][1]]]
+        ans = [[[0, 1], [1, 1]], [[1, 0], [1, 1]]]
 
     elif table == [[0, 1],
                    [1, 0]]:
-        ans = [[table[0][1]], [table[1][0]]]
+        ans = [[[0, 1]], [[1, 0]]]
 
     elif table == [[1, 0],
                    [0, 0]]:
-        ans = [[table[0][0]]]
+        ans = [[0, 0]]
 
     elif table == [[1, 0],
                    [0, 1]]:
-        ans = [[table[0][0]], [table[1][1]]]
+        ans = [[[0, 0]], [[1, 1]]]
 
     elif table == [[1, 0],
                    [1, 0]]:
-        ans = [[table[0][0], table[0][1]]]
+        ans = [[0, 0], [1, 0]]
 
     elif table == [[1, 0],
                    [1, 1]]:
-        ans = [[table[0][0], table[1][0]], [table[1][0], table[1][1]]]
+        ans = [[[0, 0], [1, 0]], [[1, 0], [1, 1]]]
 
     elif table == [[1, 1],
                    [0, 0]]:
-        ans = [[table[0][0], table[0][1]]]
+        ans = [[0, 0], [0, 1]]
 
     elif table == [[1, 1],
                    [0, 1]]:
-        ans = [[table[0][0], table[0][1]], [table[0][1], table[1][1]]]
+        ans = [[[0, 0], [0, 1]], [[0, 1], [1, 1]]]
 
     elif table == [[1, 1],
                    [1, 0]]:
-        ans = [[table[0][0], table[0][1]], [table[0][0], table[1][0]]]
+        ans = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
     elif table == [[1, 1],
                    [1, 1]]:
-        ans = [[table[0][0], table[0][1], table[1][0], table[1][1]]]
+        ans = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
     return ans
 
-print(get_2by2([[0,1],[1,1]]))
+print(get_2by2([[0,1],
+                [0,1]]))
 
 
